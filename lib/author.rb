e4ae89ee97d0914da1974795c8eaa7e8a18d0ff8
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-12 23:52:48 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,7 +10,7 @@ class Author
 
   def add_post(post)
     @posts << post
-    post.author = self 
+    post.author = self
   end
 
 end
---
+++ lib/post.rb
@@ -0,0 +1,8 @@
+
+class Post
+
+  def initialize(post)
+    @post = post
+  end
+    
+end 
