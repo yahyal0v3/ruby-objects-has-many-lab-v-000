a7baf8e80d838a443622d530b1f450495b640832
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-13 00:23:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,6 +3,8 @@ class Author
   attr_accessor :name
   attr_reader :posts
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
@@ -10,13 +12,19 @@ class Author
 
   def add_post(post)
     @posts << post
+    @@post_count += 1
     post.author = self
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
     @posts << post
+    @@post_count += 1
     post.author = self
+  end
+
+  def post_count
+    @@post_count
   end
 
 end
